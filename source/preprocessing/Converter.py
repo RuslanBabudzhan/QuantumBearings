1b8e8efaad2ec69eb8d4be4d5f24fe278945e99f
RuslanBabudzhan <ruslanbabudzhan@gmail.com>
2021-10-27 19:59:13 +0300

    Refactoring

--- source/preprocessing/Converter.py
+++ source/preprocessing/Converter.py
@@ -1,11 +1,11 @@
 from os import listdir
 from os.path import isfile, join
-from threading import excepthook
+
 import scipy.io
 import pandas as pd
 import numpy as np
-from typing import List, Optional
 from tqdm import tqdm
+
 
 class Converter:
 
@@ -14,7 +14,6 @@ class Converter:
         file_names = (file for file in listdir(path) if isfile(join(path, file)))
         for name in file_names:
             yield name
-
 
     @classmethod
     def cesar_convert(cls, path: str) -> pd.DataFrame:
@@ -32,20 +31,20 @@ class Converter:
 
             for key, col in zip(exp.keys(), ['a1_y', 'a2_y']):
                 rec_df[col] = pd.DataFrame(exp[key])
-            
-            rec_df['target'], rec_df['rpm'], rec_df['experiment_id'] = record.split('_')[4], record.split('_')[1], int(record.split('_')[0])
-            time = int(len(rec_df)/sample_rate)
-            rec_df['timestamp'] = np.linspace(0, time, sample_rate*time)
+
+            rec_df['target'], rec_df['rpm'], rec_df['experiment_id'] = record.split('_')[4], record.split('_')[1], int(
+                record.split('_')[0])
+            time = int(len(rec_df) / sample_rate)
+            rec_df['timestamp'] = np.linspace(0, time, sample_rate * time)
             data = pd.concat([data, rec_df], axis=0)
 
-        data['target'] = np.where(data['target'] == 'F0' , 1, 0) ### List
+        data['target'] = np.where(data['target'] == 'F0', 1, 0)  # List
 
         return data.reset_index(inplace=False)
 
-
     @classmethod
     def luigi_convert(cls, path: str) -> pd.DataFrame:
-        
+
         keys_to_remove = ['__header__', '__version__', '__globals__']
         sample_rate = 51200
         time = 10
@@ -60,12 +59,12 @@ class Converter:
 
             for key in keys_to_remove:
                 del exp[key]
-            
+
             rec_df[cols] = pd.DataFrame(exp[record.replace('.mat', '')])
             rec_df['target'], rec_df['rpm'], rec_df['experiment_id'] = record.split('_')[0], record.split('_')[1], i
-            rec_df['timestamp'] = np.linspace(0, time, sample_rate*time)
+            rec_df['timestamp'] = np.linspace(0, time, sample_rate * time)
             data = pd.concat([data, rec_df], axis=0)
 
-        data['target'] = np.where(data['target'] == 'C0A' , 1, 0)
+        data['target'] = np.where(data['target'] == 'C0A', 1, 0)
 
         return data.reset_index(inplace=False)
