e75eea3f686ec6e55ed96786793f403dbb2a814e
Krasii-Danylo <krasiydm@gmail.com>
2021-10-27 17:39:09 +0300

    Fixed

--- source/processes/Shuffler.py
+++ source/processes/Shuffler.py
@@ -1,70 +1,112 @@
+from typing import Union, Generator
 import numpy as np
 import pandas as pd
-from typing import Union, List
-from abc import ABC, abstractmethod
 
 
-class Shuffler(ABC):
+def id_shuffler(data: Union[pd.DataFrame, np.ndarray],
+                train_size: float) -> np.ndarray:
+    """
+    Shuffle given array into train and test datasets
 
-    @abstractmethod
-    def split(self,
-              id_0: Union[pd.DataFrame, np.ndarray], 
-              id_1: Union[pd.DataFrame, np.ndarray]):
-        pass
-    
-    def __init__(self, 
-                 train_size: float, 
+    data: array of indices
+    train_size: size of train dataset
+    """
+    len_data = len(data)
+    N = int(len_data * train_size)
+    data = np.array(data)
+    np.random.shuffle(data)
+    train, test = data[:N], data[N:]
+
+    return train, test
+
+
+class OverlapGroupCV():
+
+    def __init__(self,
+                 train_size: float,
                  n_repeats: int = 100):
 
-        assert 0 <= train_size <= 1, 'Train size should be between 0 and 1' 
+        assert 0 <= train_size <= 1, 'Train size should be between 0 and 1'
         self.train_size = train_size
         self.n_repeats = n_repeats
 
+    def split(self,
+              X: Union[pd.DataFrame, np.ndarray],
+              y: Union[pd.DataFrame, np.ndarray],
+              groups: Union[pd.DataFrame, np.ndarray]) -> Generator:
+        """
+        Create index generator for cross-validation
 
-    def id_shuffler(self, 
-                    labels: Union[pd.DataFrame, np.ndarray], 
-                    train_size: float):
-        
-        len_size = len(labels)
-        N = int(len_size * train_size)
-        labels = np.array(labels)
-        np.random.shuffle(labels)
-        train, test = labels[:N], labels[N:]
+        groups: data['experiment_id']
+        """
+        assert len(y) == len(groups), (
+            "Length of the predictors is not"
+            "matching with the groups.")
 
-        return train, test
-        
+        id_train_array, id_test_array = [], []
 
-class OverlapGroupCV(Shuffler):
-
-    def split(self, 
-              id_0: Union[pd.DataFrame, np.ndarray], 
-              id_1: Union[pd.DataFrame, np.ndarray]):
-
-        # id_train_array, id_test_array = [], []
+        # Separate defected and new bearings id
+        status = pd.DataFrame([y, groups]).T 
+        id_0 = status[status[0] == 0][1].unique()
+        id_1 = status[status[0] == 1][1].unique()
 
         for _ in range(self.n_repeats):
-
-            id_0_train, id_0_test = self.id_shuffler(id_0, self.train_size)
-            id_1_train, id_1_test = self.id_shuffler(id_1, self.train_size)
+            
+            # Stratified slpit into train and test
+            id_0_train, id_0_test = id_shuffler(id_0, self.train_size)
+            id_1_train, id_1_test = id_shuffler(id_1, self.train_size)
 
             id_train = np.concatenate([id_0_train, id_1_train])
             id_test = np.concatenate([id_0_test, id_1_test])
 
-            # id_train_array.append(id_train), id_test_array.append(id_test)
-            
-            yield id_train, id_test
-    
+            # Indexes that belong to given train and test id
+            id_train = status[status[1].map(
+                lambda x: x in id_train)].index.to_list()
+            id_test = status[status[1].map(
+                lambda x: x in id_test)].index.to_list()
 
-class PresplitedOverlapGroupCV(Shuffler):
+            id_train_array.append(id_train), id_test_array.append(id_test)
+
+        # Generator
+        for train, test in zip(id_train_array, id_test_array):
+            yield train, test
+
+
+class PresplitedOverlapGroupCV():
+
+    def __init__(self,
+                 subset_size: float = 0.63,
+                 n_repeats: int = 100):
+
+        assert 0 <= subset_size <= 1, 'Train size should be between 0 and 1'
+        self.subset_size = subset_size
+        self.n_repeats = n_repeats
 
     def split(self,
-              labels: Union[pd.DataFrame, np.ndarray]):
-        
-        # id_sub_array= []
-        
+              X: Union[pd.DataFrame, np.ndarray],
+              y: Union[pd.DataFrame, np.ndarray],
+              groups: Union[pd.DataFrame, np.ndarray],
+              train_groups: Union[pd.DataFrame, np.ndarray],
+              test_groups: Union[pd.DataFrame, np.ndarray]) -> Generator:
+
+        id_train_array, id_test_array = [], []
+
+        status = pd.DataFrame([y, groups]).T
+
         for _ in range(self.n_repeats):
+            
+            # Reduce size of given train and test groups to necessary
+            id_train, _ = id_shuffler(train_groups, self.subset_size)
+            id_test, _ = id_shuffler(test_groups, self.subset_size)
 
-            id_sub, _ = self.id_shuffler(labels, self.train_size)
-            # id_sub_array.append(id_sub)
+            # Indexes that belong to given train and test id
+            subset_train_id = status[status[1].map(
+                lambda x: x in id_train)].index.to_list()
+            subset_test_id = status[status[1].map(
+                lambda x: x in id_test)].index.to_list()
 
-            yield id_sub
+            id_train_array.append(subset_train_id), id_test_array.append(subset_test_id)
+
+        # Generator
+        for train, test in zip(id_train_array, id_test_array):
+            yield train, test
